f96e61e751f1e1f37c1724a744dcf034d65af00d
mareanalitica <mareanalitica@gmail.com>
2023-10-10 12:59:12 -0300

    docs: swagger-config

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Product",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -11,7 +20,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Produtos");
+    });
 }
 
 app.UseCors(options =>
